b76c1f6af5fc09a5eeab4bff3c5ccfef31750e05
UeEmon <kozoueda95@gmail.com>
2022-04-17 15:01:21 +0900

    Add files via upload

---
+++ botui/head.php
@@ -0,0 +1,4 @@
+<?php
+    <link rel="stylesheet" type="text/css" href="../../plugin/DataTables/datatables.min.css"/>
+	<script type="text/javascript" language="javascript" src="../../plugin/jquery/jquery-3.6.0.slim.min.js"></script>
+	<script type="text/javascript" language="javascript" src="../../plugin/DataTables/datatables.min.js"></script>
